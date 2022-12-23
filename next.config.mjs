a2611a2959bf70e0fccfb6fe776d8d672b0303f8
farisamirmudin <farisamirmudin@gmail.com>
2022-12-24 02:15:21 +0800

    added image domain

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const config = {
     defaultLocale: "en",
   },
   images: {
-    domains: ["imagecdn.me", "dracdn.com"]
+    domains: ["imagecdn.me", "dracdn.com", "gogocdn.net"]
   }
 };
 export default config;
