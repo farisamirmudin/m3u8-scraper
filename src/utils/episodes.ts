a5623e5093ed82a1fe7287b931024b3a54489560
farisamirmudin <farisamirmudin@gmail.com>
2022-12-23 23:04:00 +0800

    changed interface name from video to show

--- src/utils/episodes.ts
+++ src/utils/episodes.ts
@@ -1,11 +1,11 @@
 import { load } from "cheerio";
-import { Video } from "./search";
+import { Show } from "./search";
 
 export const episodes = async (path: string) => {
   const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL + path);
   const html = await res.text();
   const $ = load(html);
-  const episodes: Video[] = [];
+  const episodes: Show[] = [];
   $("ul.listing.items.lists li").each((i, el) => {
     const path = $(el).find("a").attr("href") ?? "";
     const img = $(el).find(".picture img").attr("src") ?? "";
