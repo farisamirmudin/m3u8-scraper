ea5b0211077a55602f511fc0e1f5f7f9e8c38d61
farisamirmudin <farisamirmudin@gmail.com>
2022-12-25 01:24:28 +0800

    changed search url

--- src/utils/fetcher/episodes.ts
+++ src/utils/fetcher/episodes.ts
@@ -7,7 +7,7 @@ export const episodes = async (path: string, type: string) => {
     const html = await res.text();
     const $ = load(html);
     const episodes: Show[] = [];
-    $("ul.listing.items.lists li").each((i, el) => {
+    $("ul.listing.items.lists li").each((_, el) => {
       const path = $(el).find("a").attr("href")?.trim() ?? "";
       const img = $(el).find(".picture img").attr("src")?.trim() ?? "";
       const name = $(el).find(".name").text().trim() ?? "";
@@ -18,22 +18,20 @@ export const episodes = async (path: string, type: string) => {
   let res = await fetch(process.env.NEXT_PUBLIC_ANIME_BASE_URL + path);
   let html = await res.text();
   let $ = load(html);
+  const title = $("div.anime_info_body_bg h1").text().trim();
   const id = $("input#movie_id").attr("value") ?? "";
   const alias = $("input#alias_anime").attr("value") ?? "";
-  const default_ep = $("input#default_ep").attr("value") ?? "";
-  const ep_start = $("a.active").attr("ep_start") ?? "";
-  const ep_end = $("a.active").attr("ep_end") ?? "";
-
-  const search_url = `https://ajax.gogo-load.com/ajax/load-list-episode?ep_start=${ep_start}&ep_end=${ep_end}&id=${id}&default_ep=${default_ep}&alias=${alias}`;
-  res = await fetch(search_url);
+  const end = $("ul#episode_page li a").last().attr("ep_end") ?? "";
+  const searchUrl = `https://ajax.gogo-load.com/ajax/load-list-episode?ep_start=0&ep_end=${end}&id=${id}&default_ep=0&alias=${alias}`;
+  const episodes: Show[] = [];
+  res = await fetch(searchUrl);
   html = await res.text();
   $ = load(html);
-  const episodes: Show[] = [];
-  $("a").each((i, el) => {
+  $("a").each((_, el) => {
     const name = $(el).find("div.name").text() ?? "";
     const path = $(el).attr("href") ?? "";
     episodes.push({
-      name,
+      name: title + " Episode " + name.split(" ").at(-1),
       path,
     });
   });
