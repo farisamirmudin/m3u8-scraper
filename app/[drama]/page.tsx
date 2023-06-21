e22250242b863e03d9cdd8667df794ff3faac5c8
Faris Amirmudin <faris.amirmudin@surialabs.com>
2023-06-22 00:30:15 +0800

    Move api logic into component since it is a server component

--- app/[drama]/page.tsx
+++ app/[drama]/page.tsx
@@ -1,4 +1,5 @@
 import { Video } from "@/typings/video";
+import { load } from "cheerio";
 import Link from "next/link";
 
 type PageType = {
@@ -13,17 +14,29 @@ export default async function Page({
   params: { drama },
   searchParams: { episode },
 }: PageType) {
-  const urlToFetch = new URL("http://localhost:3000/api/dramas/episodes");
-  urlToFetch.searchParams.set("drama", `${drama}-episode-${episode}`);
-  const episodes = await fetch(urlToFetch)
-    .then((res) => res.json())
-    .then((data) => data as Video[]);
+  // const urlToFetch = new URL("http://localhost:3000/api/dramas/episodes");
+  // urlToFetch.searchParams.set("drama", `${drama}-episode-${episode}`);
+  // const episodes = await fetch(urlToFetch)
+  //   .then((res) => res.json())
+  //   .then((data) => data as Video[]);
+  const res = await fetch(
+    `http://asianplay.net/videos/${drama}-episode-${episode}`
+  );
+  const html = await res.text();
+  const $ = load(html);
+  const episodes = [] as Video[];
+  $("ul.listing.items.lists li").each((_, el) => {
+    const path = $(el).find("a").attr("href")?.trim() ?? "";
+    const img = $(el).find(".picture img").attr("src")?.trim() ?? "";
+    const title = $(el).find(".name").text().trim() ?? "";
+    episodes.push({ title, path, img });
+  });
   return (
     <div className="">
       <p className="text-lg mb-8">Episodes</p>
 
       <div className="episode-grid">
-        {(episodes ?? []).map((ep) => {
+        {episodes.map((ep) => {
           const regex = /videos\/(.*)-episode-\d+/;
           const dramaName = (regex.exec(ep.path) ?? [])[1];
           const selectedEpisode = ep.title.split(" ").at(-1) ?? "1";
