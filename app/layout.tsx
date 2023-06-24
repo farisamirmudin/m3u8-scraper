98bd8cff9775c69e400cfd406d101f46fb7b20bd
Faris Amirmudin <faris.amirmudin@surialabs.com>
2023-06-24 10:51:38 +0800

    Add selection history

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import History from "./History";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -16,7 +17,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="mx-auto max-w-4xl p-12">{children}</div>
+        <div className="lg:grid lg:grid-cols-4 gap-4 min-h-screen flex flex-col ">
+          <aside className="p-6">
+            <History />
+          </aside>
+          <div className="col-span-3 p-6">{children}</div>
+        </div>
       </body>
     </html>
   );
